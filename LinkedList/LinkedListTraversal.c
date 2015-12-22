6809e534e3aaec35a14a1f03bbfe726a7e3398e1
Meerebrise <1440683471@qq.com>
2015-12-22 18:51:38 +0800

    Fix some little Bugs

--- LinkedList/LinkedListTraversal.c
+++ LinkedList/LinkedListTraversal.c
@@ -9,7 +9,8 @@ struct Node
    struct Node* next ;	
 } ;
 
-//Function to print the contents of the Linked List from the given node
+//Function to print the contents of the Linked List 
+//from the given node
 void printList(struct Node* ptr)
 {
    while(ptr != NULL)
@@ -26,9 +27,9 @@ int main()
 	struct Node* second = NULL ;
     struct Node* third = NULL ;
 
-    head = (struct Node*)malloc(sizeof(struct node));
-    second = (struct Node*)malloc(sizeof(struct node));
-    third = (struct Node*)malloc(sizeof(struct node));
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
 
     head->data = 1 ;
     head->next = second ;
@@ -37,7 +38,7 @@ int main()
     second->next = third ;
 
     third->data = 1 ;
-    third->next = null ;
+    third->next = NULL ;
 
     printList(head) ;
 
