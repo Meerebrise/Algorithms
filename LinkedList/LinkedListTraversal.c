1a7a6831e144237cfd4de43f0fd61b3167b47c16
Meerebrise <1440683471@qq.com>
2015-12-22 18:40:30 +0800

    Linked List Traversal

---
+++ LinkedList/LinkedListTraversal.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Codes from the website of geeksforgeeks thx for it.
+//A simple C program for traversal of a linked list.
+struct Node
+{
+   int data ;
+   struct Node* next ;	
+} ;
+
+//Function to print the contents of the Linked List from the given node
+void printList(struct Node* ptr)
+{
+   while(ptr != NULL)
+   {
+	  printf(" %d ",ptr->data) ;
+   }
+}
+
+
+int main()
+{
+
+	struct Node* head = NULL ;
+	struct Node* second = NULL ;
+    struct Node* third = NULL ;
+
+    head = (struct Node*)malloc(sizeof(struct node));
+    second = (struct Node*)malloc(sizeof(struct node));
+    third = (struct Node*)malloc(sizeof(struct node));
+
+    head->data = 1 ;
+    head->next = second ;
+
+    second->data = 1 ;
+    second->next = third ;
+
+    third->data = 1 ;
+    third->next = null ;
+
+     printList(head) ;
+
+
+
+
+	return 0;
+}
